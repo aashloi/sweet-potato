62d3fd627ec05973dcc999ac485bf30bf9360502
aashloi <3688335051@qq.com>
2025-11-09 18:21:37 +0800

    Update main.lua

--- Trouble/main.lua
+++ Trouble/main.lua
@@ -6319,7 +6319,7 @@ am.Tabs={}
 
 if am.SearchBarEnabled then
 if not ar then
-ar=ah("Search...","search",am.UIElements.Menu,nil,function(aw)
+ar=ah("搜索...","search",am.UIElements.Menu,nil,function(aw)
 for ax,ay in next,am.Tabs do
 if string.find(string.lower(ay.Name),string.lower(aw),1,true)then
 ay.UIElements.TabItem.Visible=true
